d42e8a11449fcff4752ac4cbc4feca807f852712
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-03-20 17:03:54 +0400

    Add files via upload

---
+++ house.py
@@ -0,0 +1,27 @@
+class House():
+    def __init__(self):
+        self.numberOfFloors = 9
+        self.numberOfEntrances = 3
+        self.numberOfApartaments = 125
+
+my_house = House()
+
+# Имитация состояния пожарных датчиков в i-й момент времени.
+# Предполагается, что в каждом подъезде, на каждом этаже стоит датчик.
+
+fire_detectors = [
+    0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0
+]
+
+# Проверяем состояние датчиков
+if 1 not in  fire_detectors:
+    print('В Багдаде ВСЁ спокойно!')
+else:
+    for k in range((my_house.numberOfEntrances * my_house.numberOfFloors) - 1):
+        if fire_detectors[k] == 1:
+            print(
+                'ВНИМАНИЕ! Пожар в ', 1 + k // my_house.numberOfFloors, 'подъезде, на ',
+                1 + k % my_house.numberOfFloors, ' этаже'
+            )
