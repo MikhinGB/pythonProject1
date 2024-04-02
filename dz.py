7f455df14f3548e7fd8f8f74b1099b4b1d2c8cb6
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-04-02 07:28:24 +0400

    Update dz.py

--- dz.py
+++ dz.py
@@ -1,36 +1,31 @@
 class Car:
     price = 1000000
 
-    def horse_powers(self, h_p):
-        self.h_p = 141
-        return self.h_p
+    def horse_powers(self):
+        return 141
 
 
 class Nissan(Car):
 
     price = 1900000
 
-    def horse_powers(self, h_p):
-        self.h_p = h_p
-        return self.h_p
+    def horse_powers(self):
+        return 160
 
 
 class Kia(Car):
     price = 900000
 
-    def horse_powers(self, h_p):
-        self.h_p = h_p
-        return self.h_p
+    def horse_powers(self):
+        return 150
 
 
 my_auto = Nissan()
 frend_auto = Kia()
 
-my_auto.horse_powers(160)
 print(my_auto.price)
-print(my_auto.h_p)
+print(my_auto.horse_powers())
 
-frend_auto.horse_powers(150)
 print(frend_auto.price)
-print(frend_auto.h_p)
+print(frend_auto.horse_powers())
 
