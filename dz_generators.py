7256b00573ffd25dfe3ac9cacb8ac658ef497430
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-04-29 09:02:12 +0400

    Add files via upload

---
+++ dz_generators.py
@@ -0,0 +1,19 @@
+def all_variants(text):
+    for i in range(len(text)):
+        for j in range(i+1, len(text)+1):
+            yield text[i:j]
+
+a = all_variants("abc")
+for k in a:
+    print(k)
+
+
+
+
+
+
+
+
+
+
+
