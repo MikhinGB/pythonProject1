c93e37d793d7510a799ed880fde2bb482879c603
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-05-02 18:45:55 +0400

    Add files via upload

--- dz_threads.py
+++ dz_threads.py
@@ -2,17 +2,22 @@ from threading import Thread
 from  time import sleep
 
 
-
-def func():
+def counter():
     for i in range(1, 11):
         print(i)
         sleep(1)
 
-th = Thread(target=func)
-th.start()
 
-for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']:
-    sleep(0.99)
-    print(i)
+def alfabet():
+    for i in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']:
+        print(i)
+        sleep(1)
 
-th.join()
+th1 = Thread(target=counter)
+th1.start()
+
+th2 = Thread(target=alfabet)
+th2.start()
+
+th1.join()
+th2.join()
