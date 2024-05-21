7fde0e4f553fdb7188341bfd8c5d47cd49073ff8
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-05-21 21:32:53 +0400

    Add files via upload

--- dz_processes.py
+++ dz_processes.py
@@ -1,66 +1,48 @@
 
 import multiprocessing
+import random
+
+class WarehouseManager:
 
 
-class WarehouseManager(multiprocessing.Process):
+    def __init__(self):
+        self.data = multiprocessing.Manager().dict()
 
-
-    def __init__(self, data, collector, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.collector = collector
-        self.requests = requests
-        self.data = data
-
-    def process_request(self, request):      # реализует запрос (действие с товаром), принимая requests-кортеж
+# реализует запрос (действие с товаром), принимая request-кортеж
+    def process_request(self, request, *args, **kwargs):
+        if request[0] not in self.data:
+            self.data[request[0]] = 0
         if request[1] == "receipt":
-            if request[0] not in self.data:
-                self.collector[request[0]] = request[2]
-            else:
-                self.collector[request[0]] += request[2]
-                print(self.collector)
+            self.data[request[0]] += request[2]
         else:
-            if self.collector[request[0]] >= request[2]:
-                self.collector[request[0]] -= request[2]
-            else:
-                print(
-                    f'Товарной позиции {request[0]} на складе меньше, чем требуется. Её всего {self.data[request[0]]}. '
-                    f'Измените объем заказа')
+            if self.data[request[0]] >= request[2]:
+                self.data[request[0]] -= request[2]
 
-
-    def run(self):     # принимает запросы и создает для каждого свой параллельный процес
-        self.requests = requests
-        self.collector = multiprocessing.Queue()
-
-        for request in self.requests:
-            request.start()
-        for request in self.requests:
-            request.join()
-        while not self.collector.empty():
-            self.data = self.collector.get()
+# принимает запросы и создает для каждого свой параллельный процесc
+    def run(self, requestes):
+        self.requestes = requestes
+        processes = []
+        for request in self.requestes:
+            multiprocessing.Process(target=self.process_request, args=request).start()
+        for request in self.requestes:
+            multiprocessing.Process(target=self.process_request, args=request).join()
 
 
 
 
 
-def main():
-    collector = multiprocessing.Queue()
+if __name__ == '__main__':
 
-# if __name__ == '__main__':
+    manager = WarehouseManager()
 
+    requestes = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 100),
+        ("product3", "receipt", 200),
+        ("product1", "shipment", 50)
+    ]
 
-        # start
+    manager.run(requestes)
 
-        # join
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 100),
-    ("product3", "receipt", 200),
-    ("product1", "shipment", 50)
-]
-
-manager = WarehouseManager()
-
-manager.run(requests)
-
-print(manager.data)
+    print(manager.data)
