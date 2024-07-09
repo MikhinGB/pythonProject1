32ed0f155e66ba48d716dfd0de961bfc2da1354b
MikhinGB <161627435+MikhinGB@users.noreply.github.com>
2024-07-09 21:12:51 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
-import module1
+import json
+
+def employees_rewrite(sort_type):
 
 
-module1.function1()
-module1.function2()
+    srt_type = sort_type.lower()
+
+    if srt_type.startswith('last'):
+        sort_type = 'lastName'
+    elif srt_type.startswith('first'):
+        sort_type = 'firstName'
+    elif srt_type.startswith('sal'):
+        sort_type = 'salary'
+    elif srt_type.startswith('depart'):
+        sort_type = 'department'
+    else:
+        raise ValueError('Bad key for sorting')
+
+
+    with open('employees.json') as file:
+        data = json.load(file)
+        employees = data.get('employees')
+
+        employees_sorted = sorted(employees, key=lambda x: x[sort_type], reverse=True) \
+            if isinstance(employees[0][sort_type], int) else sorted(employees, key=lambda x: x[sort_type])
+
+        data = {'employees': employees_sorted}
+        # print(data)
+
+        file_out = 'employees_' + srt_type + '_sorted.json'
+        with open(file_out, 'w') as f:
+            json.dump(data, f, indent=4)
+
+employees_rewrite('department')
